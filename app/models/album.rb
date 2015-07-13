25ae2efbe9b4d7ffceee8c81e1032b5ab43bdc58
Andrew T. poe <techapoe@gmail.com>
2015-07-13 18:56:05 -0400

    Completed tests for primary models.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,4 @@
 class Album < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: true
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -3,12 +3,21 @@ require 'test_helper'
 class AlbumTest < ActiveSupport::TestCase
 
   test "ideal conditions work" do
+    Album.create(title: "test_title")
+    founda = Album.find_by(title: "test_title")
+    assert founda
+    assert_equal founda.class, Album
   end
 
   test "can not create without title" do
+    a = Album.new(title: nil)
+    refute a.valid?
   end
 
   test "album title must be unique" do
+    Album.create(title: "test_title")
+    a = Album.new(title: "test_title")
+    refute a.valid?
   end
 
 end
